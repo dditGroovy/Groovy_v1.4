52bfd6c3d50c36d529f2a1c7403d5601340739f4
kangseoju <kangseoju@naver.com>
2023-09-26 15:55:18 +0900

    test

--- src/main/java/kr/co/groovy/MyApp.java
+++ src/main/java/kr/co/groovy/MyApp.java
@@ -17,28 +17,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyApp {
     @GetMapping("/aws")
     public String getInfo (Model model){
-        // 환경 변수에서 AWS 액세스 키와 시크릿 키 읽어오기
-        String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
-        String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+//        // 환경 변수에서 AWS 액세스 키와 시크릿 키 읽어오기
+//        String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
+//        String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+////
+////        // AWS 자격 증명 객체 생성
+//        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+//        log.info(accessKey);
+//        log.info(secretKey);
+////        // AWS S3 클라이언트 생성
+//        AmazonS3 s3Client = AmazonS3ClientBuilder
+//                .standard()
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                .withRegion(Regions.AP_NORTHEAST_2) // AWS 지역 설정
+//                .build();
 //
-//        // AWS 자격 증명 객체 생성
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        log.info(accessKey);
-        log.info(secretKey);
-//        // AWS S3 클라이언트 생성
-        AmazonS3 s3Client = AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.AP_NORTHEAST_2) // AWS 지역 설정
-                .build();
-
-        // AWS S3 서비스 사용
-        // 예: 버킷 목록 가져오기
-        final String[] name = new String[1];
-        s3Client.listBuckets().forEach(bucket -> {
-            name[0] =  bucket.getName();
-        });
-        model.addAttribute("key", name[0]);
+//        // AWS S3 서비스 사용
+//        // 예: 버킷 목록 가져오기
+//        final String[] name = new String[1];
+//        s3Client.listBuckets().forEach(bucket -> {
+//            name[0] =  bucket.getName();
+//        });
+        model.addAttribute("key", System.getenv());
         return "aws";
     }
 }
