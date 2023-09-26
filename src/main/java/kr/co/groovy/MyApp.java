daf774e2c1bd0241327a1118a54e3e66b60ac413
kangseoju <kangseoju@naver.com>
2023-09-26 22:36:47 +0900

    http 외않되!!!

--- src/main/java/kr/co/groovy/MyApp.java
+++ src/main/java/kr/co/groovy/MyApp.java
@@ -1,5 +1,9 @@
 package kr.co.groovy;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -22,29 +26,21 @@ public class MyApp {
     @GetMapping("/aws")
     public String getInfo(Model model) {
         // 환경 변수에서 AWS 액세스 키와 시크릿 키 읽어오기
-        String accessKey = env.getProperty("AWS_ACCESS_KEY_ID");
-        Map<String, String> envVariables = System.getenv();
-//        String secretKey = env.getProperty("AWS_SECRET_ACCESS_KEY");
-////
-////        // AWS 자격 증명 객체 생성
-//        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-//        log.info(accessKey);
-//        log.info(secretKey);
-////        // AWS S3 클라이언트 생성
-//        AmazonS3 s3Client = AmazonS3ClientBuilder
-//                .standard()
-//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-//                .withRegion(Regions.AP_NORTHEAST_2) // AWS 지역 설정
-//                .build();
-//
-//        // AWS S3 서비스 사용
-//        // 예: 버킷 목록 가져오기
-//        final String[] name = new String[1];
-//        s3Client.listBuckets().forEach(bucket -> {
-//            name[0] = bucket.getName();
-//        });o
-        model.addAttribute("key", accessKey);
-        model.addAttribute("env", envVariables);
+        DefaultAWSCredentialsProviderChain credentialsProvider = DefaultAWSCredentialsProviderChain.getInstance();
+
+        // Amazon S3 클라이언트 생성
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentialsProvider.getCredentials()))
+                .build();
+
+
+        // AWS S3 서비스 사용
+        // 예: 버킷 목록 가져오기
+        final String[] name = new String[1];
+        s3Client.listBuckets().forEach(bucket -> {
+            name[0] = bucket.getName();
+        });
+        model.addAttribute("env", name);
         return "aws";
     }
 }
