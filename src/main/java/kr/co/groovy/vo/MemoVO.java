158b792147673cc6c4b2a8c1a933601b6618c0c7
Yun-HaNeul <qnrty12@gmail.com>
2023-10-02 21:02:26 +0900

    memoVO에 포맷추가

--- src/main/java/kr/co/groovy/vo/MemoVO.java
+++ src/main/java/kr/co/groovy/vo/MemoVO.java
@@ -2,6 +2,7 @@ package kr.co.groovy.vo;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,6 +15,7 @@ public class MemoVO {
 	private String memoEmplId;
 	private String memoSj;
 	private String memoCn;
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd", timezone = "Asia/Seoul")
 	private Date memoWrtngDate;
 	private String commonCodeFixingAt;
 }
