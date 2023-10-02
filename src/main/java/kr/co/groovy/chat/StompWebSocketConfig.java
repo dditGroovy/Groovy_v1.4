5155f6c69ecad6812b83f1eefa4102719c8b5e64
soyeonlee <leesoyeon7038@gmail.com>
2023-10-02 15:50:29 +0900

    웹소켓 에러 잡기

--- src/main/java/kr/co/groovy/chat/StompWebSocketConfig.java
+++ src/main/java/kr/co/groovy/chat/StompWebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:8080").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("http://125.138.66.183:8080/").withSockJS();
     }
 
     @Override
